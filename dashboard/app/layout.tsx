9b8ead710c1dbbc11618ed3ef7c93ae340829370
Aaron Alvarez <aaronialvarez@yahoo.com>
2026-04-01 19:19:13 -0700

    Adding Google Analytics to the watch pages v2

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,16 +14,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KL4FM3L62J"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-KL4FM3L62J');
-      </script>
+      <body>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KL4FM3L62J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KL4FM3L62J');
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
